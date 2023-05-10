e039256d951a05701f487336702a274ca1895654
toha2102 <toha2102@student.miun.se>
2023-05-10 17:16:56 +0200

    Only unsuccessful requests are managed by ratelimit

--- middleware/rateLimiter.ts
+++ middleware/rateLimiter.ts
@@ -3,11 +3,13 @@ import { rateLimit } from 'express-rate-limit';
 export const loginLimiter15m = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 5,
-  message: 'Too many login attempts in a short time.'
+  message: 'Too many login attempts in a short time.',
+  skipSuccessfulRequests: true
 });
 
 export const loginLimiter24h = rateLimit({
   windowMs: 24 * 60 * 60 * 1000, // 24 hours
   max: 20,
-  message: 'Too many login attempts in 24 hours.'
+  message: 'Too many login attempts in 24 hours.',
+  skipSuccessfulRequests: true
 });
